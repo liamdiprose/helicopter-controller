944ad8fa7706b36b399785801c336db42fd757f1
Liam Diprose <ldi30@uclive.ac.nz>
2017-03-21 19:29:57 +1300

    Added standard hash-includes

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 /*
  * main.c
  */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/tm4c123gh6pm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+#define PART_TM4C123GH6PM
+
 int main(void) {
 	
+
+
+
 	return 0;
 }
