90df9524f5cc7269efc3043b997c8c3113a3db5a
liam <ldi30@uclive.ac.nz>
2017-05-25 19:40:58 +1200

    Heres those adc modules you wanted

---
+++ adc.c
@@ -0,0 +1,56 @@
+/*
+ * AltitudeADC.c
+ *
+ *  Created on: May 7, 2017
+ *      Author: and22
+ */
+#include "adc.h"
+
+uint32_t g_adc_current;
+
+void adc_init(void){
+	SysCtlPeripheralEnable(ADC_GPIO_PERIPH);
+	SysCtlPeripheralEnable(ADC_ADC_PERIPH);
+
+	GPIOPinTypeADC(ADC_GPIO_PERIPH, ADC_GPIO_PIN);
+
+	ADCSequenceStepConfigure(ADC_ADC_BASE, 3, 0, ADC_CTL_CH9 | ADC_CTL_IE | ADC_CTL_END);
+	ADCSequenceEnable(ADC_ADC_BASE, 3);
+
+	uint32_t altitude_hold[0]=0;
+	g_adc_current = 0;
+	SysTickIntRegister(adc_update_routine);
+	//uint32_t bottom_altitude_hold[0]=0;
+	//uint32_t bottom_altitude_current=0;
+    // TODO: Move calibration to own procedure in main.c
+//	for (uint32_t i = 0; i < 8; i++) {
+//		ADCProcessorTrigger(ADC0_BASE, 3);
+//        // Wait until ADC is ready
+//		//while(!ADCIntStatus(ADC0_BASE, 3, false));
+//		//ADCIntClear(ADC0_BASE, 3);
+//		ADCSequenceDataGet(ADC0_BASE, 3, bottom_altitude_hold);
+//		//bottom_altitude += bottom_altitude_hold[0];
+//		SysCtlDelay(22);
+//	}
+
+	//bottom_altitude= bottom_altitude/8;
+	//top_altitude=bottom_altitude-1000;
+	//altitude_range = bottom_altitude - top_altitude;
+}
+
+void adc_update_routine(void) {
+	//ADCIntClear(ADC0_BASE, 3);
+	ADCProcessorTrigger(ADC0_BASE, 3);
+	//while(!ADCIntStatus(ADC0_BASE, 3, false));
+	ADCSequenceDataGet(ADC0_BASE, 3, altitude_hold);
+
+    uint32_t old_value = g_adc_current;
+    uint32_t new_value = altitude_hold[0];
+
+	g_adc_current = old_value * ALT_OLD_RATIO + new_value * ALT_NEW_RATIO;
+}
+
+// Get the latest ADC value in percentage
+uint32_t adc_get_percent(void) {
+        return g_adc_current;
+}
---
+++ adc.h
@@ -0,0 +1,44 @@
+/*
+ * AltitudeADC.h
+ *
+ *  Created on: May 7, 2017
+ *      Author: and22
+ */
+
+#ifndef ALTITUDEADC_H_
+#define ALTITUDEADC_H_
+
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "utils/ustdlib.h"
+
+
+#define ALT_NEW_RATIO 0.7
+#define ALT_OLD_RATIO 0.3
+
+#define ADC_GPIO_PERIPH SYSCTL_PERIPH_GPIOE
+#define ADC_ADC_PERIPH SYSCTL_PERIPH_ADC0
+#define ADC_GPIO_BASE GPIO_PORTE_BASE
+#define ADC_GPIO_PIN GPIO_PIN_4
+#define ADC_ADC_BASE ADC0_BASE
+
+// Initialize the peripherals and variables required for altitute
+void adc_init(void);
+
+// Interrpt to be registered
+void adc_update_routine(void);
+
+// Get the latest ADC value in percentage 
+uint32_t adc_get_percent(void);
+
+
+#endif /* ALTITUDEADC_H_ */
+
+
