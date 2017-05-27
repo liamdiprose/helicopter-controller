60c88471312dbdc1076a51f714b20e7e4aecd15e
Liam Diprose <ldi30@uclive.ac.nz>
2017-05-27 20:19:25 +1200

    Initail implementation of PID

--- pid.h
+++ pid.h
@@ -12,26 +12,19 @@
 
 
 typedef struct PID_config_s {
-	float P;
-	float I;
-	float D;
-	float target;
+	float KP;
+	float KI;
+	float KD;
+	float I_error;
+	float D_error;
 } PIDConfig;
 
 // Create a new PID configuration
 PIDConfig pid_init(float KP, float KI, float KD);
 
-// Set Proportional Gain (KP) of PID configuration
-int pid_proportional_set(PIDConfig* config, float KP);
-
-// Set Intergral Gain (KI) of PID configuration
-int pid_intergral_set(PIDConfig* config, float KI);
-
-// Set Derivative Gain (KD) of PID configuration
-int pid_derivative_set(PIDConfig* config, float KD);
 
 // Calculate new duty cycle output from PID controller, from target and
 // current reading.
-float pid_update(PIDConfig* config, uint32_t target, uint32_t current);
+float pid_update(PIDConfig* config, uint32_t error, float dt);
 
 #endif /* PID_H_ */
